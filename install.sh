0c929eeaca1a171fc1e98446f3533ce74ac6d6ab
Ivory Hunt <162629748+Ivory-Hnt@users.noreply.github.com>
2025-09-08 17:19:21 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+cp -r * /usr/local/bin
+rm /usr/local/bin/readme.md
+chmod +x /usr/local/bin/edul
+echo
+echo Edul has been sucessfully installed
+echo 
+echo Please execute "edul -l" to login to your account
+echo
